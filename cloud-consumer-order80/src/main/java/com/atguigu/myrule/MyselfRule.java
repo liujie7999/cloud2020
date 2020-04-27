da39f8640d2ebe3720f9c624228cd150b19f0664
liujie799 <1926497487@qq.com>
2020-04-27 22:52:28 +0800

    zookeeper和Consul注册中心机制

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/myrule/MyselfRule.java
@@ -0,0 +1,20 @@
+package com.atguigu.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author liujie
+ * @Date 2020/4/27 22:14
+ * @Describe
+ */
+@Configuration
+public class MyselfRule {
+
+    @Bean
+    public IRule myRule(){
+        return new RandomRule(); //定义为随机
+    }
+}
--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -1,12 +1,18 @@
 package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.lb.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -17,6 +23,7 @@ public class OrderController {
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
+        log.info("*******消费者启动创建订单*******");
         return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
     }
 
@@ -29,4 +36,38 @@ public class OrderController {
     public CommonResult<Payment> listPayment(){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/list",CommonResult.class);
     }
+
+
+    @GetMapping("/consumer/payment/getEntity/{id}")
+    public CommonResult<Payment> getForEntity(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> forEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if ((forEntity.getStatusCode().is2xxSuccessful())){
+            return forEntity.getBody();
+        }else {
+            return new CommonResult<>(444,"调用失败");
+        }
+    }
+
+
+    /**
+     *
+     */
+    @Resource
+    private LoadBalancer loadBalancer;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping(value = "/consumer/payment/lb")
+    public  String getPaymentLB(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if(instances==null||instances.size()<0){
+            return null;
+        }
+        ServiceInstance instances1 = loadBalancer.instances(instances);
+        URI uri = instances1.getUri();
+        return restTemplate.getForObject(uri+"/payment/lb",String.class);
+    }
+
+
 }
